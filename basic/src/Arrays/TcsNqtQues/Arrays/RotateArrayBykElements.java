77236af8b7903b7af97f0dea6cbafa020e8522a0
jay gupta <jayesh.gupta3224@gmail.com>
2024-04-15 23:14:06 +0530

    solved rotate array by k by brute force

--- basic/src/Arrays/TcsNqtQues/Arrays/RotateArrayBykElements.java
+++ basic/src/Arrays/TcsNqtQues/Arrays/RotateArrayBykElements.java
@@ -4,14 +4,32 @@ import java.util.Arrays;
 
 public class RotateArrayBykElements {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 6};
-        int k = 3;
-        rotateArray(arr, arr.length, k);
+        int[] arr = {1, 2, 3, 4, 5};
+        int k = 2;
+        rotateArrayByLeft(arr, arr.length, k);
         System.out.println(Arrays.toString(arr));
 
     }
 
     static void rotateArray(int[] arr, int n, int k) {
+        for (int i = 0; i < k; i++) {
+            int temp = arr[n - 1];
+            for (int j = 0; j < n-1; j++) {
+                arr[n - 1 - j] = arr[n - 2 - j];
+            }
+            arr[0] = temp;
+        }
+    }
+//gfg ques
+static void rotateArrayByLeft(int[] arr, int n, int k) {
+    for (int i = 0; i < k; i++) {
+        int temp = arr[0];         //0
+        for (int j = 0; j < n-1; j++) {
+            arr[ j] = arr[j+1];
+        }
+        arr[n-1] = temp;      //arr[n-1]
 
     }
+}
+
 }
